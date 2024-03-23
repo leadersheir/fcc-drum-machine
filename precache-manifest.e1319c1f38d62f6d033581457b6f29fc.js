fa6f5b7dbf44023b78c2b856d2ad6d442e4b41ca
Warisul Imam <warisulimam@gmail.com>
2024-03-23 20:08:43 +0600

    Updates

---
+++ precache-manifest.e1319c1f38d62f6d033581457b6f29fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ebe3a8f09e2d92e1dc2d61d4a03dc68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26ce96ab234f12fd1681",
+    "url": "/static/css/main.8d2bcabc.chunk.css"
+  },
+  {
+    "revision": "9c00d0284eaa0ace7228",
+    "url": "/static/js/2.6a3814b4.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "/static/js/2.6a3814b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ce96ab234f12fd1681",
+    "url": "/static/js/main.e6deae08.chunk.js"
+  },
+  {
+    "revision": "2fe7576068d206c77ea0",
+    "url": "/static/js/runtime-main.b0895ed6.js"
+  }
+]);
